e8a607027033aa47b119f1e726d228581d329c33
camilo velasquez <cvagriffindor@hotmail.com>
2013-10-02 16:28:43 -0500

    este es el primer paso

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locxue::Application.config.secret_token = 'd73b7fd50909cfae2fb8b8d0507d749038bd12ab9884d46c96f610f4c807a0a61fba34f3124cab06188cf44ff0681de939ae2829bcca8baeffefd830c9b0fe46'
